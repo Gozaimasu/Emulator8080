f8ad9c561aa7599e6d2b41d40b8cb56c07212558
Gozaimasu <olivier.dahon@gmail.com>
2023-12-02 21:01:21 +0100

    code: Abstraction of the debug output
    Creation of an interface IDebugOutput and an implementation ConsoleDebugOutput to print the debug information to the console

---
+++ ConsoleEmulator/ConsoleDebugOutput.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleEmulator;
+
+internal class ConsoleDebugOutput : IDebugOutput
+{
+    public void Write(string message)
+    {
+        Console.Write(message);
+    }
+
+    public void Write(string messageFormat, object? arg0)
+    {
+        Console.WriteLine(messageFormat, arg0);
+    }
+
+    public void Write(string messageFormat, object? arg0, object? arg1)
+    {
+        Console.WriteLine(messageFormat, arg0, arg1);
+    }
+
+    public void WriteLine(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public void WriteLine()
+    {
+        Console.WriteLine();
+    }
+}
