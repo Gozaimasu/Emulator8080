3159876d8d4f186dbf7b7fb2fcec01da679e82c5
Gozaimasu <olivier.dahon@gmail.com>
2023-12-02 15:25:28 +0100

    code: Changes to ConditionCodes

--- ConsoleEmulator/ConditionCodes.cs
+++ ConsoleEmulator/ConditionCodes.cs
@@ -1,11 +1,20 @@
-﻿namespace ConsoleEmulator;
+﻿using System.Runtime.InteropServices;
 
+namespace ConsoleEmulator;
+
+[StructLayout(LayoutKind.Explicit, Size = 8)]
 internal struct ConditionCodes
 {
-    public byte Z { get; set; }
-    public byte S { get; set; }
-    public byte P { get; set; }
-    public byte CY { get; set; }
-    public byte AC { get; set; }
-    public byte PAD { get; set; }
+    [FieldOffset(0)]
+    public byte Z;
+    [FieldOffset(1)]
+    public byte S;
+    [FieldOffset(2)]
+    public byte P;
+    [FieldOffset(3)]
+    public byte CY;
+    [FieldOffset(4)]
+    public byte AC;
+    [FieldOffset(5)]
+    public byte PAD;
 }
