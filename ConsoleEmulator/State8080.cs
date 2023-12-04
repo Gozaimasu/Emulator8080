22477cc4c64ce7ee3330d093f12782a9e3d517b1
Olivier DAHON <odahon@users.noreply.github.com>
2023-12-04 13:03:09 +0100

    code : Registers are stored in a byte[]

--- ConsoleEmulator/State8080.cs
+++ ConsoleEmulator/State8080.cs
@@ -2,16 +2,61 @@
 
 internal struct State8080
 {
-    public byte A { get; set; }
-    public byte B { get; set; }
-    public byte C { get; set; }
-    public byte D { get; set; }
-    public byte E { get; set; }
-    public byte H { get; set; }
-    public byte L { get; set; }
+    private readonly byte[] _registers  = new byte[8];
+
+    public byte A
+    {
+        get => _registers.AsSpan()[7];
+        set => _registers.AsSpan()[7] = value;
+    }
+    public byte B
+    {
+        get => _registers.AsSpan()[0];
+        set => _registers.AsSpan()[0] = value;
+    }
+    public byte C
+    {
+        get => _registers.AsSpan()[1];
+        set => _registers.AsSpan()[1] = value;
+    }
+    public byte D
+    {
+        get => _registers.AsSpan()[2];
+        set => _registers.AsSpan()[2] = value;
+    }
+    public byte E
+    {
+        get => _registers.AsSpan()[3];
+        set => _registers.AsSpan()[3] = value;
+    }
+    public byte H
+    {
+        get => _registers.AsSpan()[4];
+        set => _registers.AsSpan()[4] = value;
+    }
+    public byte L
+    {
+        get => _registers.AsSpan()[5];
+        set => _registers.AsSpan()[5] = value;
+    }
     public ushort SP { get; set; }
     public ushort PC { get; set; }
-    public byte[] Memory { get; set; }
+    public byte[] Memory { get; set; } = [];
     public ConditionCodes CC { get; set; }
     public byte IntEnable { get; set; }
+
+    public State8080()
+    {
+        
+    }
+
+    public byte GetRegister(int offset)
+    {
+        return _registers.AsSpan()[offset];
+    }
+
+    public void SetRegister(int offset, byte value)
+    {
+        _registers.AsSpan()[offset] = value;
+    }
 }
