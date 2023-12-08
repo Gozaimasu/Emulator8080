56ece6416327f2506ef88af43afcf794d36f9052
Gozaimasu <olivier.dahon@gmail.com>
2023-12-08 22:14:54 +0100

    test: Emulate PUSH (B, D, H)

--- ConsoleEmulator.Test/HelperTests.Other.cs
+++ ConsoleEmulator.Test/HelperTests.Other.cs
@@ -18,6 +18,78 @@ public partial class HelperTests
         Assert.Equal(expectedOutput, debugOutput.Output);
     }
 
+    [Fact]
+    public void Emulate8080Op_WhenPUSHB_ShouldSucceed()
+    {
+        // Arrange
+        State8080 state = new()
+        {
+            Memory = [0xC5, 0x00, 0x00],
+            B = 0x02,
+            C = 0x01,
+            PC = 0,
+            SP = 3
+        };
+
+        // Act
+        int done = Helper.Emulate8080Op(ref state);
+
+        // Assert
+        Assert.Equal(0, done);
+        Assert.Equal(1, state.PC);
+        Assert.Equal(0x02, state.Memory[2]);
+        Assert.Equal(0x01, state.Memory[1]);
+        Assert.Equal(1, state.SP);
+    }
+
+    [Fact]
+    public void Emulate8080Op_WhenPUSHD_ShouldSucceed()
+    {
+        // Arrange
+        State8080 state = new()
+        {
+            Memory = [0xD5, 0x00, 0x00],
+            D = 0x02,
+            E = 0x01,
+            PC = 0,
+            SP = 3
+        };
+
+        // Act
+        int done = Helper.Emulate8080Op(ref state);
+
+        // Assert
+        Assert.Equal(0, done);
+        Assert.Equal(1, state.PC);
+        Assert.Equal(0x02, state.Memory[2]);
+        Assert.Equal(0x01, state.Memory[1]);
+        Assert.Equal(1, state.SP);
+    }
+
+    [Fact]
+    public void Emulate8080Op_WhenPUSHH_ShouldSucceed()
+    {
+        // Arrange
+        State8080 state = new()
+        {
+            Memory = [0xE5, 0x00, 0x00],
+            H = 0x02,
+            L = 0x01,
+            PC = 0,
+            SP = 3
+        };
+
+        // Act
+        int done = Helper.Emulate8080Op(ref state);
+
+        // Assert
+        Assert.Equal(0, done);
+        Assert.Equal(1, state.PC);
+        Assert.Equal(0x02, state.Memory[2]);
+        Assert.Equal(0x01, state.Memory[1]);
+        Assert.Equal(1, state.SP);
+    }
+
     [Theory]
     [MemberData(nameof(DisassembleTestData.GetPOPData), MemberType = typeof(DisassembleTestData))]
     public void Disassemble8080Op_WhenPOP_ShouldSucceed(byte[] data, string expectedOutput)
