80aabc58614b23135cdfe3188d8a7da6e3c77646
Gozaimasu <olivier.dahon@gmail.com>
2023-12-26 17:24:50 +0100

    code : WinForm project added

---
+++ WinFormsEmulator/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WinFormsEmulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            loadFileToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog1 = new OpenFileDialog();
+            pictureBox1 = new PictureBox();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { loadFileToolStripMenuItem });
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(125, 20);
+            toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // loadFileToolStripMenuItem
+            // 
+            loadFileToolStripMenuItem.Name = "loadFileToolStripMenuItem";
+            loadFileToolStripMenuItem.Size = new Size(121, 22);
+            loadFileToolStripMenuItem.Text = "Load File";
+            loadFileToolStripMenuItem.Click += loadFileToolStripMenuItem_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(196, 27);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(256, 224);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem loadFileToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ WinFormsEmulator/Form1.cs
@@ -0,0 +1,94 @@
+using ConsoleEmulator;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace WinFormsEmulator;
+
+public partial class Form1 : Form
+{
+    private readonly CPU8080 _cpu;
+    private readonly System.Timers.Timer _cpuTimer;
+    private readonly System.Timers.Timer _screenTimer;
+    private readonly Stopwatch _stopwatch = new();
+    private long _lastCpuStep = 0;
+
+    public Form1()
+    {
+        _cpu = new CPU8080();
+        _cpuTimer = new()
+        {
+            Interval = 1
+        };
+        _cpuTimer.Elapsed += OnCpuTimer;
+        _screenTimer = new()
+        {
+            Interval = TimeSpan.FromMicroseconds(16666).TotalMilliseconds
+        };
+        _screenTimer.Elapsed += OnScreenTimer;
+
+        InitializeComponent();
+    }
+
+    private void OnScreenTimer(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        _screenTimer.Enabled = false;
+
+        byte[] vram = new byte[7168];
+        Array.Copy(_cpu.Memory, 0x2400, vram, 0, 7168);
+        Array.Reverse(vram);
+
+        IntPtr ptr = Marshal.UnsafeAddrOfPinnedArrayElement(vram, 0);
+        using (Bitmap bmp = new(256, 224, 32, System.Drawing.Imaging.PixelFormat.Format1bppIndexed, ptr))
+        {
+            bmp.RotateFlip(RotateFlipType.Rotate90FlipNone);
+            pictureBox1.Image = (Image)bmp.Clone();
+        }
+
+        _screenTimer.Enabled = true;
+    }
+
+    private void OnCpuTimer(object? sender, System.Timers.ElapsedEventArgs e)
+    {
+        _cpuTimer.Enabled = false;
+
+        long elapsed = _stopwatch.ElapsedMilliseconds;
+        long diff = elapsed - _lastCpuStep;
+
+        // 2 MHz = 2000 cycles per milliseconds
+        long cyclesToRun = 2000 * diff;
+        cyclesToRun += _cpu.Cycles;
+        while (_cpu.Cycles < cyclesToRun)
+        {
+            int done = _cpu.Step();
+            if (done != 0)
+            {
+                _cpuTimer.Stop();
+                _screenTimer.Stop();
+            }
+        }
+
+        _lastCpuStep = elapsed;
+        _cpuTimer.Enabled = true;
+    }
+
+    private void loadFileToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+        var result = openFileDialog1.ShowDialog();
+        if (result != DialogResult.OK)
+        {
+            return;
+        }
+        byte[] buffer = new byte[0x4000];
+        using (Stream romStream = openFileDialog1.OpenFile())
+        {
+            romStream.Read(buffer, 0, buffer.Length);
+        }
+        _cpu.Init(buffer, 0);
+
+        _cpuTimer.Start();
+        _screenTimer.Start();
+        _stopwatch.Reset();
+        _lastCpuStep = 0;
+        _stopwatch.Start();
+    }
+}
